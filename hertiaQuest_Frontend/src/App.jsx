4c907f3cb23a0d1cb220fc2852419eaa3e684b01
rohit-0000 <rohit20040121@gmail.com>
2025-07-03 00:25:56 +0530

    fixing bug

--- hertiaQuest_Frontend/src/App.jsx
+++ hertiaQuest_Frontend/src/App.jsx
@@ -159,6 +159,23 @@ function App() {
       path: "/oauth2/redirect",
       element: <OAuth2RedirectHandler />,
     },
+    {
+      path: "*",
+      element: (
+        <div>
+          <h1
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              minHeight: "100vh",
+            }}
+          >
+            This Page does not Exist
+          </h1>
+        </div>
+      ),
+    },
   ]);
   useEffect(() => {
     if (token != null) {
